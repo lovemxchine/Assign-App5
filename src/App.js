f808e8f32e9ed942f10ca66631461bfac3759435
lovemxchine <lovemxchine@gmail.com>
2024-08-19 11:15:41 +0700

    add useState

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 // import { EventData1 } from "./event-data";
 import RefsFunc from "./refs"
+import FuncCompo from "./func-component";
 export default function App() {
-  return <RefsFunc />;
+  return <FuncCompo />;
 }
---
+++ src/func-component.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+
+const FuncCompo =()=> {
+    let [text,setText] = useState('Hello World')
+    let [size,setSize] = React.useState(16)
+
+    const onClickSetText = ()=> {
+        let t = prompt('กำหนดข้อความ')
+        if(t){
+            setText(t)
+        }
+    }
+    const onClickZoomIn = () =>{
+        let newSize = size + 1 
+        setSize(newSize)
+    }
+    let msgboxStyle = {
+        display : 'inline-block',
+        width:350,
+        fontSize:size,
+        backgroundColor:'#ccc',
+        padding:5,
+        textAlign:'left',
+    }
+
+    return (
+        <div style = {{textAlign:'center',marginTop:20}}>
+            <div style={msgboxStyle}>{text}</div><br/><br/>
+            <button onClick={onClickSetText}>ข้อความ</button>&nbsp;
+            <button onClick={onClickZoomIn}>text</button>&nbsp;
+            <button onClick={()=> setSize(size-1)}>size</button>
+
+
+        </div>
+    )
+
+}
+
+export default FuncCompo
